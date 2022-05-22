dcae01be3b61de5390829b0fcb6e904d93657458
Olga Serova <serova.home@gmail.com>
2022-05-22 03:00:12 +0300

    01

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,42 +1,56 @@
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class App {
 
+    static public List<String> readFileByFilter(String fileName, String filter) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        ArrayList<String> list = new ArrayList<>();
+        while (reader.ready()) {
+            String tmp = reader.readLine();
+            if (tmp.contains(filter)) {
+                list.add(tmp);
+            }
+        }
+        reader.close();
+        return list;
+    }
 
+    static public void listFilesForFolder(final File folder) throws IOException {
 
-    static public void listFilesForFolder(final File folder) {
-
-        ArrayList<File> arrayList = new ArrayList<>();
+        LinkedList<File> list = new LinkedList<>();
 
         for (final File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
                 listFilesForFolder(fileEntry);
             } else {
-                arrayList.add(fileEntry);
-                //System.out.println(fileEntry.getPath());
+                list.add(fileEntry);
             }
         }
 
-        arrayList.sort(Comparator.comparing(File::getName).reversed());
+        list.sort(Comparator.comparing(File::getName).reversed());
+        //Collections.sort(list, Comparator.comparing(File::getAbsolutePath));
 
-        for (File file : arrayList) {
+
+        for (File file : list) {
             System.out.println(file.getPath());
+            List<String> listStr = readFileByFilter(file.getPath(), "require ");
+            for (String str : listStr)
+            {
+                System.out.println(str);
+            }
         }
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         final File folder = new File("src/main/resources");
         listFilesForFolder(folder);
 
-
-//        File dir = new File("src/main/resources"); //path указывает на директорию
-//        File[] arrFiles = dir.listFiles();
-//        List<File> lst = Arrays.asList(arrFiles);
 
 
     }

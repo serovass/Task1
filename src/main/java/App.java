e07e3866a1a367ca543f3f99ddef3cec9f320648
Olga Serova <serova.home@gmail.com>
2022-05-22 16:19:21 +0300

    to replace

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,6 +6,8 @@ import java.util.*;
 
 public class App {
 
+    static ArrayList<File> list = new ArrayList<>();
+
     static public List<String> readFileByFilter(String fileName, String filter) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(fileName));
         ArrayList<String> list = new ArrayList<>();
@@ -21,8 +23,6 @@ public class App {
 
     static public void listFilesForFolder(final File folder) throws IOException {
 
-        ArrayList<File> list = new ArrayList<>();
-
         for (final File fileEntry : folder.listFiles()) {
             if (fileEntry.isDirectory()) {
                 listFilesForFolder(fileEntry);
@@ -31,39 +31,7 @@ public class App {
             }
         }
 
-        list.sort(Comparator.comparing(File::getName).reversed());
 
-
-        for (int i = 0; i < list.size(); ++i) {
-            File file = list.get(i);
-            System.out.println(file.getPath());
-            List<String> listStr = readFileByFilter(file.getPath(), "require ");
-            for (String str : listStr)
-            {
-                String require = str.substring(str.indexOf('‘') + 1, str.indexOf('’'));
-                File ifExist = new File("src/main/resources/" + require);
-                if (ifExist.exists())
-                {
-                    System.out.println(require);/////
-                    //File fileTmp = file.
-                    for (int j = i; j < list.size(); ++j)
-                    {
-                        if (list.get(j).getPath().equals("src/main/resources/" + require))
-                        {
-                            System.out.println("YES");
-                            break;
-                        }
-
-                        if (j + 1 == list.size())
-                        {
-                            System.out.println("NO!");
-                        }
-                    }
-
-                }
-
-            }
-        }
     }
 
 
@@ -71,6 +39,44 @@ public class App {
 
         final File folder = new File("src/main/resources");
         listFilesForFolder(folder);
+
+
+        list.sort(Comparator.comparing(File::getName));
+        for (File file : list)
+        {
+            System.out.println(file.getPath());
+        }
+        for (int i = 0; i < list.size(); ++i) {
+
+            System.out.println(list.get(i).getPath());
+            List<String> listStr = readFileByFilter(list.get(i).getPath(), "require ");
+            for (String str : listStr)
+            {
+                String require = str.substring(str.indexOf('‘') + 1, str.indexOf('’'));
+                File ifExist = new File("src/main/resources/" + require);
+
+                if (ifExist.exists())
+                {
+                    System.out.print(require);/////
+                    int j;
+                    for (j = i; j < list.size(); j++)
+                    {
+                        if (list.get(j).getPath().equals("src/main/resources/" + require))
+                        {
+                            System.out.println(" - TO REPLACE");
+                            break;
+                        }
+                    }
+
+                    if (j == list.size())
+                    {
+                        System.out.println(" - OK ");
+                    }
+
+                }
+
+            }
+        }
 
     }
 
